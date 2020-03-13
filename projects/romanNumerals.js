a32e9bb1fb46f9e7dc2f68101276173e5656892a
Tavita Su'a <tavitas@sprep.org>
2020-03-13 15:19:46 +1400

    started on roman numerlas challenge

---
+++ projects/romanNumerals.js
@@ -0,0 +1,19 @@
+/**
+ * Convert the given number into a roman numeral.
+ * All roman numerals answers should be provided in upper-case.
+ */
+
+function convertToRoman(num) {
+  const I = 1;
+  const V = 5;
+  const X = 10;
+  const L = 50;
+  const C = 100;
+  const D = 500;
+  const M = 1000;
+
+  return num;
+}
+ 
+console.log(convertToRoman(36));
+ 
